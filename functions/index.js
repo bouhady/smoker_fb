e3168b0358c1af83f39d993dd0c0d0c4f0865a1a
Yaniv Bouhadana <yaniv.bouhadana@citi.com>
2019-06-20 15:48:09 +0300

    add FCM push notifications add temperature boundaries

--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,32 @@ admin.initializeApp();
 
 
 exports.multiTempUpdate = functions.https.onRequest((request, response) => {
-  var temperture1 = request.param("t1", 0)
-  var temperture2 = request.param("t2", 0)
+  var temperture1 = Number(request.param("t1", 0))
+  var temperture2 = Number(request.param("t2", 0))
   var date = new Date().valueOf();
   var tempertures = {t1:temperture1, t2:temperture2};
   pushMultiTemp(date, tempertures)
+  admin.database().ref('/messageEnabled').once('value').then(function (snapshot) {
+    var messageEnabled = snapshot.val()
+    if (messageEnabled == 1) {
+      admin.database().ref('/tempertureBounderies').once('value').then(function (snapshot2) {
+        var bunderies = snapshot2.val()
+        if (temperture1<bunderies.min) {
+          console.log(' MIN Tempertures triggered : t1' + temperture1 + " t2 :" + temperture2 + " min :" + bunderies.min + " max :" + bunderies.max);
+          pushDataToSend("Smoker getting cold","T1 : " + temperture1 + " T2 : " + temperture2);
+          setMessageEnable(0);
+        } else {
+          if (temperture1>bunderies.max) {
+            console.log('MAX Tempertures triggered : t1' + temperture1 + " t2 :" + temperture2 + " min :" + bunderies.min + " max :" + bunderies.max);
+            pushDataToSend("Smoker getting Hot","T1 : " + temperture1 + " T2 : " + temperture2);
+            setMessageEnable(0);
+          } 
+        }
+      });
+    } else {
+
+    }
+  })
   response.send("Hellochild " + temperture1 + " " + date + " Added ");
 });
 
@@ -83,6 +104,22 @@ function resetAllTableUntilDate(ref) {
     });
 }
 
+exports.send = functions.https.onRequest((request, response) => {
+
+  setMessageEnable(request.param("en", "0"));
+  admin.database().ref('/tempertureBounderies').once('value').then(function (snapshot2) {
+    var bunderies = snapshot2.val()
+    pushDataToSend("Smoker Notification On ","From " + bunderies.min + " to  " + bunderies.max);
+  });
+  response.send("setMessageEnable ");
+});
+
+
+function pushDataToSend(title,body) {
+  var datatosend = {name:title, text: body};
+  var ref = admin.database().ref('/messages')
+  ref.push(datatosend)
+}
 exports.historyMultiTemp = functions.https.onRequest((req, res) => {
   var lastSec = req.param("lastSec", "1000")
   var tableSource = req.param("table", "/multiTempData/")
@@ -146,8 +183,53 @@ exports.historyMultiTemp = functions.https.onRequest((req, res) => {
       <div id="chart_div" style="width: 900px; height: 500px;"></div>
     </body>
   </html>`);
+  setMessageEnable(1);
     });
 
 
 });
 // [END all]
+
+
+
+// Listens for new messages added to messages/:pushId
+exports.pushNotification = functions.database.ref('/messages/{pushId}').onWrite(  (change,context) => {
+  console.log('Push notification event triggered');
+  var valueObject = change.after.val();
+  if(valueObject.photoUrl != null) {
+    valueObject.photoUrl= "Sent you a photo!";
+  }
+  const payload = {
+      notification: {
+          title:valueObject.name,
+          body: valueObject.text || valueObject.photoUrl,
+          sound: "default"
+      },
+  };
+  const options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+  };
+  return admin.messaging().sendToTopic("pushNotifications", payload, options);
+});
+
+function setMessageEnable(enable) {
+  var ref = admin.database().ref('/messageEnabled')
+  ref.set(enable)
+  if (enable == 1) {
+    pushDataToSend("Smoker App", "Smoker alerts on");
+  }
+}
+
+function setTemperturesBounderies(bounderies) {
+  var ref = admin.database().ref('/tempertureBounderies')
+  ref.update(bounderies)
+}
+
+exports.updateBounderies = functions.https.onRequest((request, response) => {
+  var tempertureMin = Number(request.param("min", 0))
+  var tempertureMax = Number(request.param("max", 0))
+  var tempertures = {min:tempertureMin, max:tempertureMax};
+  setTemperturesBounderies(tempertures)
+  response.send("tempertures updated  ");
+});
